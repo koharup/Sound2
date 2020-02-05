a2b50a1152d92766ee82f6e8957450f3ef5baf93
koharup <anandpita18@icloud.com>
2020-02-05 17:47:40 +0900

    サウンドアプリ2完成

--- app/src/main/java/app/sano/picchi/sound/MainActivity.kt
+++ app/src/main/java/app/sano/picchi/sound/MainActivity.kt
@@ -3,6 +3,7 @@ package app.sano.picchi.sound
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,12 +14,30 @@ class MainActivity : AppCompatActivity() {
 
         val drumSound  = MediaPlayer.create(this, R.raw.drum_sound)
 
+        drumImageView.setOnTouchListener { view, event ->
 
-        drumImageView.setOnClickListener {
-            //音を巻き戻す
-            drumSound.seekTo(0)
-            drumSound.start()
+            if (event.action == MotionEvent.ACTION_DOWN){
+
+                drumImageView.setImageResource(R.drawable.drum_playing_image)
+                drumSound.seekTo(0)
+                drumSound.start()
+
+
+                }
+            else if (event.action == MotionEvent.ACTION_UP){
+                drumImageView.setImageResource(R.drawable.drum_image)
+            }
+
+            true
 
         }
+
+//         サウンドアプリ1の内容
+//        drumImageView.setOnClickListener {
+//            //音を巻き戻す
+//            drumImageView.setImageResource(R.drawable.drum_playing_image)
+//            drumSound.seekTo(0)
+//            drumSound.start()
+//        }
     }
 }
