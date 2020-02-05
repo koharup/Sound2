4dd7abdff5de54f0ad0da68474268f4c76886e3a
koharup <anandpita18@icloud.com>
2020-02-05 16:17:20 +0900

    太鼓のみ音なるだけ

---
+++ app/src/main/java/app/sano/picchi/sound/MainActivity.kt
@@ -0,0 +1,23 @@
+package app.sano.picchi.sound
+
+import android.media.MediaPlayer
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val drumSound  = MediaPlayer.create(this, R.raw.drum_sound)
+
+
+        drumImageView.setOnClickListener {
+            //音を巻き戻す
+            drumSound.seekTo(0)
+
+        }
+    }
+}
